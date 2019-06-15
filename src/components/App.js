fc655a012bc39cd65ef356d9e52007c68be85708
Victor Liu <victorliu716@gmail.com>
2019-06-15 18:49:24 -0500

    Style h1 title

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Dashboard from './Dashboard'
 
+const divStyle = {
+  color: 'orange'
+};
+
+
 class App extends Component {
     render() {    
       return (      
       <div>
-         <h1>Semux</h1> 
+         <h1 style={divStyle}>Semux</h1> 
          <div>
            <Dashboard/>
          </div>
